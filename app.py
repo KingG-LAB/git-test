d4b557ae111e937694fd8a4f05799776951ceb03
KingG-LAB <rhrlfgns89@gmail.com>
2026-04-01 14:23:58 +0900

    Test Jenkins webhook auto trigger

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def home():
     return jsonify(
         {
-            "message": "DevOps backend webhook test",
+            "message": "DevOps backend webhook abcd test",
             "status": "ok",
         }
     )
